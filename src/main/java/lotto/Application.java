8de1c79caae7e7223b7845f7efa4d23f4d7da89d
choijw1004 <97choijw@gmail.com>
2025-11-03 02:04:56 +0900

    feat: Application 로직 연결

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,28 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.controller.LottoController;
+import lotto.service.LottoMatchingService;
+import lotto.service.LottoService;
+import lotto.service.StatisticsService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        try{
+            LottoController lottoController = new LottoController(
+                    new LottoService(),
+                    new LottoMatchingService(),
+                    new StatisticsService(),
+                    new InputView(),
+                    new OutputView()
+            );
+            lottoController.run();
+        } finally {
+            Console.close();
+        }
     }
 }
